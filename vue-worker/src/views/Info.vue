bc150921b4bd9545c9f05e13deedff2b4ee74e0f
RJY <junyeren@outlook.com>
2024-05-08 22:02:56 +0800

    perf(vue-worker:info.vue):table was taken place by echarts

--- vue-worker/src/views/Info.vue
+++ vue-worker/src/views/Info.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <div class="main">
 
     <el-table v-loading="loading" :data="orders" style="width: 100%">
@@ -110,4 +110,114 @@ export default {
   margin-bottom: 0;
   width: 100%;
 }
-</style>
+</style> -->
+
+<template>
+  <div id="chart" style="width: 1400px; height: 750px;"></div>
+</template>
+
+<script>
+import * as echarts from 'echarts';
+
+export default {
+  name: 'Chart',
+  mounted() {
+    this.initChart()
+  },
+  methods: {
+    initChart() {
+      const chart = echarts.init(document.getElementById('chart'))
+
+      const option = {
+        title: {
+          text: '订单信息统计'
+        },
+        tooltip: {
+          trigger: 'axis'
+        },
+        legend: {
+          data: ['销量','评论', '热度']
+        },
+        xAxis: {
+          type: 'category',
+          data: ['我的姐姐','我们一起摇太阳','人潮汹涌', '哥斯拉大战金刚', '功夫熊猫4','第二十条','红雀', '你好，李焕英','灿烂的她', '刺杀小说家'],
+          axisLabel: {
+            inside: false,
+            color: 'black',
+            textstyle: {
+              fontSize: '100',
+              itemSize:'',
+            }
+          },
+          axisTick: {
+            show: true
+          },
+          axisLine: {
+            show: true
+          },
+        },
+        yAxis: {
+          type: 'value',
+          axisLine: {
+            show: false
+          },
+          axisTick: {
+            show: false
+          },
+          axisLabel: {
+            color: '#999'
+          }
+        },
+        dataZoom: [
+          {
+            type: 'inside'
+          }
+        ],
+        series: [
+          {
+            name: '销量',
+            type: 'bar',
+            data: [100, 14, 87, 45, 61, 3, 37, 90, 56, 26],
+            type: 'bar',
+            showBackground: true,
+            itemStyle: {
+              color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+                { offset: 0, color: '#83bff6' },
+                { offset: 0.5, color: '#188df0' },
+                { offset: 1, color: '#188df0' }
+              ])
+            },
+            emphasis: {
+              itemStyle: {
+                color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+                  { offset: 0, color: '#2378f7' },
+                  { offset: 0.7, color: '#2378f7' },
+                  { offset: 1, color: '#83bff6' }
+                ])
+              }
+            },
+          },
+          {
+            name: '评论',
+            type: 'bar',
+            emphasis: {
+              focus: 'series'
+            },
+            data: [66, 12, 40, 21, 30,1,15,46,24,11]
+          },
+          {
+            name: '热度',
+            type: 'bar',
+            emphasis: {
+              focus: 'series'
+            },
+            data: [166, 26, 127, 66, 91,4,52,136,80,37]
+          }
+        ]
+      }
+
+      chart.setOption(option)
+    }
+  }
+}
+</script>
